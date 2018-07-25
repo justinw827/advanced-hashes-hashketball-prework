e7363e19edc70901630cb5bdd57e9894d785e3b5
Justin Wang <justinw827@aol.com>
2018-07-25 21:54:54 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,8 +201,11 @@ def most_points_scored
   topScorer = " "
   max = 0
   players.each do |name|
-    if
+    if num_points_scored(name) > max
+      topScorer = name
+    end
   end
+  topScorer
 end
 
 puts team_names
